5719ffdff8c72e51b56ac94e4dccc3b69f404e68
ArtemZaZ <temka.911@mail.ru>
2018-05-04 18:34:47 +0300

    Исправил ошибки, добавил возможность регулировать частоту опроса новопришедших событий

--- test.py
+++ test.py
@@ -4,19 +4,19 @@ from RTCEventMaster import *
 def firstfun():
     i = 0
     while i < 10:
-        print("first" + i)
+        print("first" + str(i))
         i = i + 1
 
 
 def secondfun():
     i = 0
     while i < 10:
-        print("second" + i)
+        print("second" + str(i))
         i = i + 1
 
 
-EvMaster = EventMaster()            # Создаем мастера событий и запускаем его
-EvMaster.start()
+EvMaster = EventMaster(freq=1)            # Создаем мастера событий и запускаем его
+
 
 evBlockFirst = EventBlock()         # Создаем блок события
 evBlockSecond = EventBlock()
@@ -24,9 +24,14 @@ evBlockSecond = EventBlock()
 evBlockFirst.setfun(firstfun)       # Привязываем ф-ию к событию
 evBlockSecond.setfun(secondfun)
 
-EventMaster.append(evBlockFirst)    # Добавляем блок события в EM
-EventMaster.append(evBlockSecond)
+EvMaster.append(evBlockFirst)    # Добавляем блок события в EM
+EvMaster.append(evBlockSecond)
+
+EvMaster.start()
 
 evBlockFirst.push()                 # Вызываем событие
 evBlockSecond.push()
 
+time.sleep(5)
+
+EvMaster.exit()
