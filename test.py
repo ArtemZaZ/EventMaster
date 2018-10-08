404cd6036058cb70aca0e6606e4a3cf9e7e207e8
ArtemZaZ <temka.911@mail.ru>
2018-10-08 22:32:37 +0300

    Обновил архитектуру

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from RTCEventMaster import *
+from eventmaster import *
+import time
 
 
 def firstfun():
@@ -26,32 +27,30 @@ def fourthfun(a, b, c, d):
     print(d, c)
 
 
-EvMaster = EventMaster(freq=20)            # Создаем мастера событий и запускаем его
-
-
-evBlockFirst = EventBlock()         # Создаем блок события
-evBlockSecond = EventBlock()
-evBlockThird = EventBlock()
-evBlockFourth = EventBlock()
-
-evBlockFirst.setfun(firstfun)       # Привязываем ф-ию к событию
-evBlockSecond.setfun(secondfun)
-evBlockThird.setfun(thirdfun)
-evBlockFourth.setfun(fourthfun)
-
-
-EvMaster.append(evBlockFirst)    # Добавляем блок события в EM
-EvMaster.append(evBlockSecond)
-EvMaster.append(evBlockThird)
-EvMaster.append(evBlockFourth)
-
+EvMaster = EventMaster()            # Создаем мастера событий и запускаем его
 EvMaster.start()
 
-evBlockFirst.push()                 # Вызываем событие
-evBlockSecond.push()
-evBlockThird.push("Ye")
+evFirst = Event()         # Создаем блок события
+evSecond = Event()
+evThird = Event()
+evFourth = Event()
+
+evFirst.connect(firstfun)       # Привязываем ф-ию к событию
+evSecond.connect(secondfun)
+evThird.connect(thirdfun)
+evFourth.connect(fourthfun)
+
+
+EvMaster.append(evFirst)    # Добавляем блок события в EM
+EvMaster.append(evSecond)
+EvMaster.append(evThird)
+EvMaster.append(evFourth)
+
+evFirst.push()                 # Вызываем событие
+evSecond.push()
+evThird.push("Ye")
 time.sleep(1)
-evBlockFourth.push("a", 3, 1, 6)
+evFourth.push("a", 3, 1, 6)
 
 time.sleep(3)
 
